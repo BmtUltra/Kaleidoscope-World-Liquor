7ac13138022aaf1adc77ca78a33d761ab40367f3
BmtUltra <1453935889@qq.com>
2026-03-29 14:09:16 +0800

    冰红茶

---
+++ src/main/java/com/bmt/kaleidoscope_world_liquor/effect/MultiJumpEffect.java
@@ -0,0 +1,22 @@
+package com.bmt.kaleidoscope_world_liquor.effect;
+
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectCategory;
+import net.minecraft.world.entity.LivingEntity;
+import org.jetbrains.annotations.NotNull;
+
+public class MultiJumpEffect extends MobEffect {
+    
+    public MultiJumpEffect() {
+        super(MobEffectCategory.BENEFICIAL, 0x00FF00);
+    }
+    
+    @Override
+    public void applyEffectTick(@NotNull LivingEntity entity, int amplifier) {
+    }
+    
+    @Override
+    public boolean isDurationEffectTick(int duration, int amplifier) {
+        return false;
+    }
+}
--- src/main/java/com/bmt/kaleidoscope_world_liquor/init/ModEffects.java
+++ src/main/java/com/bmt/kaleidoscope_world_liquor/init/ModEffects.java
@@ -2,6 +2,7 @@ package com.bmt.kaleidoscope_world_liquor.init;
 
 import com.bmt.kaleidoscope_world_liquor.KaleidoscopeWorldLiquor;
 import com.bmt.kaleidoscope_world_liquor.effect.CaptainGiftEffect;
+import com.bmt.kaleidoscope_world_liquor.effect.MultiJumpEffect;
 import com.bmt.kaleidoscope_world_liquor.effect.TequilaEffect;
 import com.bmt.kaleidoscope_world_liquor.effect.TreasureGuideEffect;
 import net.minecraft.world.effect.MobEffect;
@@ -22,6 +23,9 @@ public class ModEffects {
 
     public static final RegistryObject<MobEffect> TREASURE_GUIDE_EFFECT = MOB_EFFECTS.register("treasure_guide",
             TreasureGuideEffect::new);
+
+    public static final RegistryObject<MobEffect> MULTI_JUMP_EFFECT = MOB_EFFECTS.register("multi_jump",
+            MultiJumpEffect::new);
 
     public static void register(IEventBus eventBus) {
         MOB_EFFECTS.register(eventBus);
